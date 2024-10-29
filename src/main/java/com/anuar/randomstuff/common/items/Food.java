ff6c38e1f4821e4ce4c9aa74336723d196866fb3
Anuar <anuarorozzco@gmail.com>
2024-10-28 21:31:28 -0600

    Created Food class and added our first item

---
+++ src/main/java/com/anuar/randomstuff/common/items/Food.java
@@ -0,0 +1,23 @@
+package com.anuar.randomstuff.common.items;
+
+import com.anuar.randomstuff.RandomStuffMain;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class Food {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RandomStuffMain.MODID);
+
+    public static final RegistryObject<Item> TACO = ITEMS.register("taco",
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(5).saturationMod(0.6F).build())));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
+
